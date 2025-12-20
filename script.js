5256193f5ddc82b9043b83685f9096697a0fd8f2
Hanna Dubyna <ann10062016@gmail.com>
2025-12-20 21:44:35 +0100

    basic layout, create header, mobile menu with toggle functionality

---
+++ script.js
@@ -0,0 +1,27 @@
+// swiper
+
+// nav open & close
+const body = document.querySelector("body"),
+  navMenu = body.querySelector(".menu-content"),
+  navOpenBtn = body.querySelector(".navOpen-btn");
+navCloseBtn = navMenu.querySelector(".navClose-btn");
+
+if (navMenu && navOpenBtn) {
+  navOpenBtn.addEventListener("click", () => {
+    navMenu.classList.add("open");
+    body.style.overflowY = "hidden";
+  });
+}
+
+if (navMenu && navOpenBtn) {
+  navCloseBtn.addEventListener("click", () => {
+    navMenu.classList.remove("open");
+    body.style.overflowY = "scroll";
+  });
+}
+
+// scroll up button
+
+// nav link indicator
+
+// scroll reveal animation
