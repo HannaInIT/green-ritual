9e2570dbca5b7359fef26fb7332af014cd6c60df
Hanna Dubyna <ann10062016@gmail.com>
2025-12-22 00:55:28 +0100

    implement swiper for home section

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// swiper js
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  // grabCursor: true,
+  // spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// nav open & close
+const body = document.querySelector("body"),
+  navMenu = body.querySelector(".menu-content"),
+  navOpenBtn = body.querySelector(".navOpen-btn");
+navCloseBtn = navMenu.querySelector(".navClose-btn");
+
+if (navMenu && navOpenBtn) {
+  navOpenBtn.addEventListener("click", () => {
+    navMenu.classList.add("open");
+    body.style.overflowY = "hidden";
+  });
+}
+
+if (navMenu && navOpenBtn) {
+  navCloseBtn.addEventListener("click", () => {
+    navMenu.classList.remove("open");
+    body.style.overflowY = "scroll";
+  });
+}
+
+// scroll up button
+
+// nav link indicator
+
+// scroll reveal animation
