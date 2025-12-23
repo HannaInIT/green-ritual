131e7836755b3424d8a9d371e20496e8e4e8f592
Hanna Dubyna <ann10062016@gmail.com>
2025-12-24 00:01:10 +0100

    add home section images and enhance swiper layout with titles and buttons

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
 // swiper js
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
   // direction: 'vertical',
   loop: true,
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   // And if we need scrollbar
   scrollbar: {
-    el: '.swiper-scrollbar',
+    el: ".swiper-scrollbar",
   },
 });
-
 
 // nav open & close
 const body = document.querySelector("body"),
