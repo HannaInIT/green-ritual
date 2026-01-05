86c24a4e7ef40f171159121379ae98aad31462e1
Hanna Dubyna <ann10062016@gmail.com>
2026-01-05 23:57:05 +0100

    feat: enhance header behavior and scroll up button visibility; update testimonial details

--- js/script.js
+++ js/script.js
@@ -44,16 +44,40 @@ if (navMenu && navOpenBtn) {
 // change header bg color
 window.addEventListener("scroll", () => {
   const scrollY = window.pageYOffset;
+
   if (scrollY > 5) {
     document.querySelector("header").classList.add("header-active");
   } else {
     document.querySelector("header").classList.remove("header-active");
   }
-  console.log(scrollY);
+
+  // scroll up button
+  const scrollUpBtn = document.querySelector(".scrollUp-btn");
+
+  if (scrollY > 250) {
+    scrollUpBtn.classList.add("scrollUpBtn-active");
+  } else {
+    scrollUpBtn.classList.remove("scrollUpBtn-active");
+  }
+
+  // nav link indicator
+  const sections = document.querySelectorAll("section[id]");
+  sections.forEach((section) => {
+    const sectionHeight = section.offsetHeight,
+      sectionTop = section.offsetTop - 60;
+
+    let navId = document.querySelector(
+      `.menu-content a[href*="${section.id}"]`
+    );
+
+    if (!navId) return;
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      navId.classList.add("active-navlink");
+    } else {
+      navId.classList.remove("active-navlink");
+    }
+    console.log(navId);
+  });
+  console.log(sections);
 });
-
-// scroll up button
-
-// nav link indicator
-
 // scroll reveal animation
