4fcdd3bb8e2ead28909136e62697a39bb74816da
Hanna Dubyna <ann10062016@gmail.com>
2026-01-08 02:58:49 +0100

    feat: implement scroll reveal animations and enhance header, about and menu styles

--- js/script.js
+++ js/script.js
@@ -85,4 +85,18 @@ window.addEventListener("scroll", () => {
 });
 
 // scroll reveal animation
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.15,
+  }
+);
 
+document.querySelectorAll(".js-reveal").forEach((el) => observer.observe(el));
